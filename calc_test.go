1009f53d71d1cac28287888c68bb54dd20d51d09
Anthony Bible <anthony@anthonybible.com>
2024-03-13 20:42:24 -0600

    Add subtract method

--- calc_test.go
+++ calc_test.go
@@ -6,3 +6,9 @@ func TestAdd(t *testing.T) {
 		t.Error("1 + 2 did not equal 3")
 	}
 }
+
+func TestSubtract(t *testing.T) {
+	if result:=Subtract(2, 1); result!= 1 {
+		t.Error("2 - 1 did not equal 1, got ", result)
+	}
+}
